36cc8765dc2985fcd4314d40f01c030670f56651
awe-sim <awesim@gmail.com>
2018-06-29 17:27:36 -0700

    [FEATURE] Adds TemplateVar directive..

---
+++ src/directives/directives.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { TemplateVarDirective } from './template-var/template-var';
+@NgModule({
+	declarations: [TemplateVarDirective],
+	imports: [],
+	exports: [TemplateVarDirective]
+})
+export class DirectivesModule {}
