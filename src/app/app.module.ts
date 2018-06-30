23f15821f2b9eddd0bdacc4be7bc146f78a8e5c0
awe-sim <awesim@gmail.com>
2018-06-29 17:15:49 -0700

    [DEV] Enables lazy loading..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,19 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 
 @NgModule({
-	declarations: [
-		MyApp,
-		HomePage
-	],
+	declarations: [ MyApp ],
 	imports: [
 		BrowserModule,
-		IonicModule.forRoot(MyApp)
+		IonicModule.forRoot(MyApp),
 	],
-	bootstrap: [IonicApp],
-	entryComponents: [
-		MyApp,
-		HomePage
-	],
+	bootstrap: [ IonicApp ],
+	entryComponents: [ MyApp ],
 	providers: [
 		StatusBar,
 		SplashScreen,
-		{provide: ErrorHandler, useClass: IonicErrorHandler}
+		{ provide: ErrorHandler, useClass: IonicErrorHandler },
 	]
 })
-export class AppModule {}
+export class AppModule { }
