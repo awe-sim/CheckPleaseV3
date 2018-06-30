bf82b491fe2db4ba8154e1714cef21dbc6c94460
awe-sim <awesim@gmail.com>
2018-06-29 17:18:49 -0700

    [FEATURE] Enables translations..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { TranslateService } from '@ngx-translate/core';
+
 @Component({
 	templateUrl : 'app.html',
 })
@@ -14,6 +16,7 @@ export class MyApp {
 		platform         : Platform,
 		statusBar        : StatusBar,
 		splashScreen     : SplashScreen,
+		public translate : TranslateService,
 	) {
 		platform.ready().then(() => {
 			// Okay, so the platform is ready and our plugins are available.
@@ -21,6 +24,12 @@ export class MyApp {
 			statusBar.styleDefault();
 			splashScreen.hide();
 		});
+		this.initTranslate();
+	}
+
+	initTranslate() {
+		this.translate.setDefaultLang('en');
+		this.translate.use('en');
 	}
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,20 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+const TRANSLATIONS_PATH_PREFIX = './assets/i18n/';
+const TRANSLATIONS_PATH_SUFFIX = '.json';
+
 @NgModule({
 	declarations: [ MyApp ],
 	imports: [
 		BrowserModule,
 		IonicModule.forRoot(MyApp),
+		HttpClientModule,
+		TranslateModule.forRoot({ loader: { provide: TranslateLoader, useFactory: (http: HttpClient) => new TranslateHttpLoader(http, TRANSLATIONS_PATH_PREFIX, TRANSLATIONS_PATH_SUFFIX), deps: [ HttpClient ] } }),
 	],
 	bootstrap: [ IonicApp ],
 	entryComponents: [ MyApp ],
